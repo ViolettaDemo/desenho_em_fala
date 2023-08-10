5b7620c6a353bcf9a92c7245c8957a8763bae5d7
ViolettaDemo <111540245+ViolettaDemo@users.noreply.github.com>
2023-08-10 14:38:35 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 x=0;
 y=0;
-var desenharcircolo="";
-var desenharquadrado="";
+var drawCircle="";
+var drawRect="";
 var SpeechRecognition = window.webkitSpeechRecognition; 
 var recognition = new SpeechRecognition();
 function start (){
@@ -18,9 +18,10 @@ drawCircle = "set";
  } if(conteudo =="retângulo") { x = Math.floor(Math.random() * 900);
 y = Math.floor(Math.random() * 600); document.getElementById("status").innerHTML = "Desenhando um retângulo."; drawRect = "set";
  } } 
- function setup() { canvas = createCanvas(900, 600); } function draw() { if(drawCircle == "set") { radius = Math.floor(Math.random() * 100);
- círculo(x, y, raio);
- document.getElementById("status").innerHTML = "UCírculo desenhado. "; drawCircle = "";
+ function setup() { canvas = createCanvas(900, 600); } function draw() {
+     if(drawCircle == "set") { raio = Math.floor(Math.random() * 100);
+ circle(x, y, raio);
+ document.getElementById("status").innerHTML = "Círculo desenhado. "; drawCircle = "";
  } if(drawRect == "set") { rect(x,y,70,50);
 document.getElementById("status").innerHTML = "Retângulo desenhado. "; 
 drawRect = ""
